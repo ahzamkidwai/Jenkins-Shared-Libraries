2e19649c2221a6afb96f15ef34d2e0f0ffdc02c8
Ahzam Naseem Kidwai <130897998+ahzamkidwai@users.noreply.github.com>
2025-07-25 13:11:04 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,12 +1,11 @@
 def call(String Project, String ImageTag, String DockerHubUser){
   withCredentials([usernamePassword(
-                    credentialsId: 'DockerHubCred',
-                    passwordVariable: "dockerHubPass", 
-                    usernameVariable: 'dockerHubUser'
-                )]){
-                    sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                    sh "docker image tag notes-app:latest ${env.dockerHubUser}/notes-app:latest"
-                    sh "docker push ${env.dockerHubUser}/notes-app:latest"    
-                }
-
+    credentialsId: 'DockerHubCred',
+    usernameVariable: 'DOCKER_HUB_USER',
+    passwordVariable: 'DOCKER_HUB_PASS'
+  )]) {
+    sh "docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PASS"
+    sh "docker image tag ${Project}:${ImageTag} $DOCKER_HUB_USER/${Project}:${ImageTag}"
+    sh "docker push $DOCKER_HUB_USER/${Project}:${ImageTag}"
+  }
 }
